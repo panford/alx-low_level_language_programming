b758fa29858ea60006408a38b4655d68ff87abb7
Kobby Panford-Quainoo <panfordkobby@gmail.com>
2022-03-13 03:53:23 +0000

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-
-int main(void) /*Main function*/
 /*
  * main - Entry point
  * Return: Always 0 (Success)
  */
+int main(void) /*Main function*/
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
