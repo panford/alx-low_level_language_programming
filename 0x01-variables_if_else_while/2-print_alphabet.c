a7ef3cf4cadbef66bcd5af08368fcba2442671ad
panford <panfordkobby@gmail.com>
2022-03-13 20:22:19 -0700

    update 1-last digit file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+/**
+ * main - entry point of the function
+ * Return: 0 if successful
+ */
+#include<stdio.h>
+
+int main(void)
+{
+  int i;
+  for (i=0; i < 26; i++)
+    {
+      putchar('A'-1+i);
+    }
+return (0);
+}
