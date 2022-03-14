d3251c432635eebaf9bc1a625bcc4c90162a4e89
panford <panfordkobby@gmail.com>
2022-03-13 21:35:35 -0700

    update 4 print alphabets file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+/**
+* main - entry point of the function
+* Return: 0 if successful
+*/
+
+#include<stdio.h>
+
+int main(void)
+{
+int i;
+int lastNum;
+lastNum = 9;
+for (i = 0; i <= lastNum; i++)
+{
+printf("%d", i)
+};
+printf("\n");
+return (0);
+}
