ead17a28733b51ba387ae30c79ff7b5b7f445846
panford <panfordkobby@gmail.com>
2022-03-13 20:52:07 -0700

    update 4 print alphabets file

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,12 +11,12 @@ int i;
 char character;
 for (i = 0; i <= 25; i++)
 {
-character = 'a' + i
+character = 'a' + i;
 if (character != 'q' || character != 'e')
 {
 putchar(character);
 };
-
+};
 putchar('\n');
 return (0);
-}
+};
