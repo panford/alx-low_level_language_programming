2403291d43e09ca17c5365636921ccb6aa11ede3
panford <panfordkobby@gmail.com>
2022-03-13 20:48:35 -0700

    update 4 print alphabets file

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+/**
+* main - entry point of the function
+* Return: 0 if successful
+*/
+
+#include<stdio.h>
+
+int main(void)
+{
+int i;
+char character;
+for (i = 0; i <= 25; i++)
+{
+character = 'a' + i
+if (character != 'q' || character != 'e')
+{
+putchar(character);
+};
+
+putchar('\n');
+return (0);
+}
