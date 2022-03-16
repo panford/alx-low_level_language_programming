afbec6a9836b99c5d969b777121478f592042525
Kobby Panford-Quainoo <panfordkobby@gmail.com>
2022-03-16 03:06:23 +0000

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 /**
  *_islower - checks for a lowercase character
  *@c: letter being tested
@@ -8,7 +8,11 @@
 int _islower(int c)
 {
 if (c >= 97 && c <= 122)
-	return (1);
+{
+return (1);
+}
 else
-	return (0);
+{
+return (0);
+};
 }
