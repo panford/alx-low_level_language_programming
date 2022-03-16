4139213837fc6cd85eef553a7d988da8a0bf1047
Kobby Panford-Quainoo <panfordkobby@gmail.com>
2022-03-16 02:55:24 +0000

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,8 +2,10 @@
  * print_alphabet - Entry point
  * Return:0
  */
- int main(void)
- {
- printf("_putchar\n");
- return (0);
- }
+#include"main.h"
+
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
